8c7a191835ba1fef723f634f1efcd27481a0550a
ahmdfauzy15 <ahfauzy15@gmail.com>
2026-03-11 10:55:11 +0700

    c

--- src/pages/EReporting.jsx
+++ src/pages/EReporting.jsx
@@ -1122,7 +1122,7 @@ const EReporting = () => {
             <BarChart3 className="w-7 h-7 text-white" />
           </div>
           <div>
-            <h1 className="text-2xl lg:text-3xl font-bold text-red-900">Sistem Pengawasan LJK EReporting {currentDateTime.getFullYear()}</h1>
+            <h1 className="text-2xl lg:text-3xl font-bold text-red-900">Monitoring Absensi EReporting </h1>
             <p className="text-gray-600 mt-1">Monitoring Pengawasan LJK Laporan EReporting - Total {stats.total} Laporan • {stats.totalLJK} LJK</p>
             <div className="flex items-center space-x-4 mt-1">
               <p className="text-sm font-medium text-gray-700 bg-white px-3 py-1 rounded-lg shadow-sm border border-gray-200">
